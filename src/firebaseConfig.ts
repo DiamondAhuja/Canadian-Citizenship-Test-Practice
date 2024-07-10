e0ba99ae0652989e3109f85a8fe15e15d71ba803
DiamondAhuja <ahujadiamond@gmail.com>
2024-07-10 16:03:40 -0400

    chore: Update firebaseConfig.ts to use dotenv for environment variable configuration

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +21,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export default app;
