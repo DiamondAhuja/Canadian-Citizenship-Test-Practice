5f72a38928be727c175bd584b590e0a4bf645adb
DiamondAhuja <ahujadiamond@gmail.com>
2024-07-12 16:42:09 -0400

    chore: Update sign-in and sign-up components

--- src/components/AnonymousSignIn.tsx
+++ src/components/AnonymousSignIn.tsx
@@ -1,22 +1,22 @@
 // AnonymousSignIn.tsx
-import React from 'react';
-import { signInAnonymously } from 'firebase/auth';
-import { auth } from '../services/firebaseConfig';
+import React from "react";
+import { signInAnonymously } from "firebase/auth";
+import { auth } from "../services/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 const AnonymousSignIn: React.FC = () => {
+  const navigate = useNavigate();
+
   const handleAnonymousSignIn = async () => {
     try {
       await signInAnonymously(auth);
+      navigate("/");
     } catch (error) {
       console.error("Error signing in anonymously: ", error);
     }
   };
 
-  return (
-    <button onClick={handleAnonymousSignIn}>
-      Continue as Guest
-    </button>
-  );
+  return <button onClick={handleAnonymousSignIn}>Continue as Guest</button>;
 };
 
 export default AnonymousSignIn;
